f3404356308f312344a94deb35cc0ae840e615ca
Meni Fontaine <menif@uoregon.edu>
2017-07-14 20:01:17 -0700

    cron job to auto-updatae central git repository for puppet

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,14 @@
-node ip-10-0-1-200 {
+node ip-10-0-1-200{
+#update /etc/puppet on puppet master 
+   cron { "puppet update":
+      command => "cd /etc/puppet && git  pull -q origin master",
+      user => root,
+      minute => "*/5",
+   }
 }
 
-node ip-10-0-1-5 {
+node ip-10-0-1-5{
 }
 
 node ip-10-0-1-164 {
 }
-
-
-
