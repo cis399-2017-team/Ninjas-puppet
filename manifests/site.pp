64c57b21c6c59daf2b9585fff77ca088b0fba09d
Meni Fontaine <menif@uoregon.edu>
2017-07-23 15:05:57 -0700

    a few updates to puppet master

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,10 @@
-node ip-10-0-1-200{
+node ip-10-0-1-169{
 #update /etc/puppet on puppet master 
    cron { "puppet update":
       command => "cd /etc/puppet && git  pull -q origin master",
       user => root,
       minute => "*/5",
    }
-
-   #iptables disable 
-   service { "iptables":
-      ensure => "stopped"
-      enable => "false" 
-   }
-
-   cron {"disable iptables and ufw"
-      command => "ufw disable",
-      user => root,
-      minute => "*/5",
-      }
 }
 
 node ip-10-0-1-5{
