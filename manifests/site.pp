daae5366d67a3ba9b060c36a91589831b680b914
Third Ninja <stevep@uoregon.edu>
2017-07-23 21:49:12 +0000

    minor changes for new master

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,22 @@
-node ip-10-0-1-169{    
+node ip-10-0-1-200{
 #update /etc/puppet on puppet master 
    cron { "puppet update":
       command => "cd /etc/puppet && git  pull -q origin master",
       user => root,
       minute => "*/5",
-  }
+   }
+
+   #iptables disable 
+   service { "iptables":
+      ensure => "stopped"
+      enable => "false" 
+   }
+
+   cron {"disable iptables and ufw"
+      command => "ufw disable",
+      user => root,
+      minute => "*/5",
+      }
 }
 
 node ip-10-0-1-5{
