14bd025b88a4fecec59b7a42df1e14195903e6b1
Meni Fontaine <menif@uoregon.edu>
2017-07-23 22:47:15 -0700

    menif_more updates

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -3,8 +3,7 @@ class apache {
     	    ensure => latest
     }
     service { "apache2":
-	    subscribe => File[indexhtml],
-	    subscribe => File[apacheconf],
+	    subscribe => File[""/etc/apache2/apache2.conf],
 	    require => Package["apache2"],
 	    ensure => "running",
 	    enable => true
