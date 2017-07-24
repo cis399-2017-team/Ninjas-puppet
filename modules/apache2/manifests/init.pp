e85acfc66784eaff600f48c3a8168bc923d7d62f
Steven Parker <stevep@uoregon.edu>
2017-07-23 19:54:10 -0700

    updated with test rules

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -2,6 +2,19 @@ class apache {
     package { "apache2":
     	    ensure => latest
     }
+    service { "apache2":
+	    subscribe => File[indexhtml],
+	    require => Package["apache2"],
+	    ensure => "running",
+	    enable => true
+    }
 
-
+    file {"indexhtml":
+	    name => "/var/www/html/index",
+	    owner => root,
+	    group => root,
+	    mode => 644,
+	    source => "puppet:///modules/apache2/index",
+	    require => Package["apache2"]
+    }
 }
