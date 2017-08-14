00ffdc2a72907d741bc5613ea697b0c6247ee0df
Steven Parker <stevep@uoregon.edu>
2017-08-14 14:13:36 -0700

    posix compliance

--- modules/fail2ban/manifests/init.pp
+++ modules/fail2ban/manifests/init.pp
@@ -1,81 +1,44 @@
 class fail2ban {
-
-    
     package { "fail2ban":
-
-        ensure => latest
-,
+        ensure => latest,
     }
-
-
     service { "fail2ban":
-
     	subscribe => File[fail2banconf],
-
         require => Package["fail2ban"],
-
         ensure => "running",
-
-	enable => true
-,
+	enable => true,
     }
-
-
     file { "fail2banconf":
-
-        name => "/etc/fail2ban/fail2ban.conf"
-,
+        name => "/etc/fail2ban/fail2ban.conf",
 	owner => root,
-
 	group => root,
-
 	mode => 644,
-
 	source => "puppet:///modules/fail2ban/fail2ban.conf",
-
-	require => Package["fail2ban"]
-,
+	require => Package["fail2ban"],
     }
-
     file { "fail2banlocal":
         name => "/etc/fail2ban/fail2ban.local",
 	owner => root,
-
 	group => root,
-
 	mode => 644,
-
 	source => "puppet:///modules/fail2ban/fail2ban.local",
-
-	require => Package["fail2ban"]
-,
+	require => Package["fail2ban"],
     }
-
     file { "jailconf":
         name => "/etc/fail2ban/jail.conf",
 	owner => root,
-
 	group => root,
-
 	mode => 644,
-
 	source => "puppet:///modules/fail2ban/jail.conf",
-
-	require => Package["fail2ban"]
-,
+	require => Package["fail2ban"],
     }
 
     file { "jaillocal":
         name => "/etc/fail2ban/jail.local",
 	owner => root,
-
 	group => root,
-
 	mode => 644,
-
 	source => "puppet:///modules/fail2ban/jail.local",
-
-	require => Package["fail2ban"]
-,
+	require => Package["fail2ban"],
     }
 }
